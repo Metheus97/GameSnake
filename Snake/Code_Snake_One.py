63d6d0841266617444864053484da08d686adbf1
Matheus Pereira <78231377+Metheus97@users.noreply.github.com>
2021-05-02 20:28:07 -0300

    Game Finished

--- Snake/Code_Snake_One.py
+++ Snake/Code_Snake_One.py
@@ -2,6 +2,7 @@ import random
 import pygame
 from pygame.locals import *
 
+#Funçoes auxiliares
 def on_grid_random():
     x = random.randint(0, 590)
     y = random.randint(0, 590)
@@ -10,7 +11,7 @@ def on_grid_random():
 def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
-
+# Definição para o movimento da cobra.
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -32,30 +33,49 @@ apple.fill((255,0,0))
 my_direction = LEFT
 
 clock = pygame.time.Clock()
+font = pygame.font.Font('freesansbold.ttf', 18)
+score = 0
 
+game_over = False
 while True:
     clock.tick(15)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
+            exit()
 
         if event.type == KEYDOWN:
-            if event.key == K_UP:
+            if event.key == K_UP and my_direction != DOWN:
                 my_direction = UP
-            if event.key == K_DOWN:
+            if event.key == K_DOWN and my_direction != UP:
                 my_direction = DOWN
-            if event.key == K_LEFT:
+            if event.key == K_LEFT and my_direction != RIGHT:
                 my_direction = LEFT
-            if event.key == K_RIGHT:
+            if event.key == K_RIGHT and my_direction != LEFT:
                 my_direction = RIGHT
 
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
         snake.append((0,0))
+        score = score + 1
+
+# Checar se a cobra colidiu com as paredes.
+    if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
+        game_over = True
+        break
+#Checar se a cobra colidiu com seu corpo.
+    for i in range(1, len(snake) - 1):
+        if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
+            game_over = True
+            break
+
+    if game_over:
+        break
 
     for i in range(len(snake) - 1, 0, -1):
         snake[i] = (snake[i - 1][0], snake[i - 1][1])
 
+#Mover a cobra.
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
     if my_direction == DOWN:
@@ -65,11 +85,34 @@ while True:
     if my_direction == LEFT:
         snake[0] = (snake[0][0] - 10, snake[0][1])
 
-
-
     screen.fill((0,0,0,))
     screen.blit(apple, apple_pos)
+
+    for x in range(0, 600, 10):
+        pygame.draw.line(screen, (40, 40, 40), (x, 0), (x, 600))
+    for y in range(0, 600, 10):
+        pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
+
+        score_font = font.render('score:%s' % (score), True, (255, 255, 255,))
+        score_rect = score_font.get_rect()
+        score_rect.topleft = (600 - 120, 10)
+        screen.blit(score_font, score_rect)
+
     for pos in snake:
         screen.blit(snake_skin, pos)
+
     pygame.display.update()
 
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf', 75)
+    game_over_screen = game_over_font.render('Game Over', True, (255, 255, 255))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect.midtop = (600 / 2, 10)
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.update()
+    pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
